7a607f661def9dcf8c20b2e0493ada16be5ab122
WAD3n <gluchowski.hubert@gmail.com>
2023-12-13 23:22:52 +0100

    #1

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+
 import requests
 import re
+import spacy
+from collections import Counter
+import matplotlib.pyplot as plt
+import sklearn
+from wordcloud import WordCloud
 
 romantycznosc = requests.get('https://wolnelektury.pl/media/book/txt/ballady-i-romanse-romantycznosc.txt')
 testament_moj = requests.get('https://wolnelektury.pl/media/book/txt/testament-moj.txt')
@@ -39,4 +45,97 @@ while ('' in slowa_cierpeienie):
 while ('—' in slowa_cierpeienie):
     slowa_cierpeienie.remove('—')
 
-print(slowa_romantycznosc)
+korpus = []
+korpus.append(slowa_romantycznosc)
+korpus.append(slowa_bogurodzica)
+korpus.append(slowa_stepy_akremanskie)
+korpus.append(slowa_cierpeienie)
+korpus.append(slowa_testament_moj)
+do_analizy = []
+lan = spacy.load('pl_core_news_lg')
+for _ in korpus:
+    do_analizy.append(lan(_))
+
+grammar_freq = []
+for i in do_analizy:
+    helper = Counter()
+    helper.update([token.pos_ for token in helper])
+    grammar_freq.append(helper)
+print(grammar_freq)
+print(grammar_freq[0].values())
+
+counter =0
+for _ in grammar_freq:
+    plt.plot(grammar_freq[_].keys(),grammar_freq[_].values())
+    plt.title(f'Frequency of grammar classes')
+    plt.xlabel('grammar class')
+    plt.ylabel('Frequency')
+    plt.savefig('wykresy/' + str(counter))
+    counter+=1
+    plt.close()
+
+
+word_form = []
+for _ in korpus:
+    ptr = Counter()
+    ptr.update([token.tag_ for holder in _])
+    word_form.append(ptr)
+
+counter+=1
+for _ in word_form:
+    plt.plot(word_form[_].keys(),word_form[_].values())
+    plt.title(f'Frequency of words form')
+    plt.xlabel('form of word')
+    plt.ylabel('Frequency')
+    plt.savefig('wykresy/' + str(counter))
+    counter+=1
+    plt.close()
+
+nouns = []
+licznik = Counter()
+for _ in korpus:
+    licznik.update([token.lemma_ for token in _ if token.pos_ == 'NOUN'])
+    nouns.append(licznik.most_common(15))
+
+counter+=1
+for _ in nouns:
+    plt.plot(nouns[_].keys(),nouns[_].values())
+    plt.title(f'Frequency of nouns')
+    plt.xlabel('noun')
+    plt.ylabel('Frequency')
+    plt.savefig('wykresy/' + str(counter))
+    counter+=1
+    plt.close()
+
+def term_frequency(dictionary):
+    sum_values = 0
+    for key,value in dictionary.items():
+        sum_values += value
+    tf = {}
+    for key,value in dictionary.items():
+        tf[key] = value/sum_values
+    return tf
+
+def inverse_document_frequency(dictionary,d2,d3,d4,d5):
+    file = open('bag-of-words.txt','r')
+    idf = {}
+    occurence_in_files = 1
+    for key,value in dictionary.items():
+        if key in d2.keys():occurence_in_files  += 1
+        if key in d3.keys(): occurence_in_files += 1
+        if key in d4.keys(): occurence_in_files += 1
+        if key in d5.keys(): occurence_in_files += 1
+        idf[key] = abs(np.log( 5 / occurence_in_files))
+        occurence_in_files = 1
+    return idf
+def tf_idf(dictionary,tf,idf):
+    tf_wartosci = list(tf.values())
+    idf_wartosci = list(idf.values())
+    ptr = {}
+    _ = 0
+    for key , value in dictionary.items():
+        ptr[key] =tf_wartosci[_] * idf_wartosci[_]
+        _ += 1
+    _ = 0
+    return ptr
+
