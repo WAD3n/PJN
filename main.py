f3bb7f0450c4215ab8d824ce8efa128b100058b6
WAD3n <96685536+WAD3n@users.noreply.github.com>
2023-12-18 16:07:20 +0100

    final form

--- main.py
+++ main.py
@@ -1,236 +1,202 @@
-import requests
-import string
-import re
-from collections import Counter
-import matplotlib.pyplot as plt
+
+import nltk
+from nltk.tokenize import word_tokenize
 import numpy as np
-import math
+from collections import Counter
+import sys
+from utils2 import sigmoid, get_batches, compute_pca, get_dict
+nltk.data.path.append('.')
+import re
+nltk.download('punkt')                                                      
+with open('shakespeare.txt') as f:
+    data = f.read()                                                 
+data = re.sub(r'[,!?;-]', '.',data)                                 
+data = nltk.word_tokenize(data)                                     
+data = [ ch.lower() for ch in data if ch.isalpha() or ch == '.']    
+print("Number of tokens:", len(data),'\n', data[:15])  
+fdist = nltk.FreqDist(word for word in data)
+print("Size of vocabulary: ",len(fdist) )
+print("Most frequent tokens: ",fdist.most_common(20) )     
+word2Ind, Ind2word = get_dict(data)
+V = len(word2Ind)
+print("Size of vocabulary: ", V)
+print("Index of the word 'king' :  ",word2Ind['king'] )
+print("Word which has index 2743:  ",Ind2word[2743] )
+def initialize_model(N,V, random_seed=1):
+    np.random.seed(random_seed)
+    W1 = np.random.rand(N, V)
+    W2 = np.random.rand(V, N)
+    b1 = np.zeros((N, 1))
+    b2 = np.zeros((V, 1))
+    return W1, W2, b1, b2
+tmp_N = 4
+tmp_V = 10
+tmp_W1, tmp_W2, tmp_b1, tmp_b2 = initialize_model(tmp_N,tmp_V)
+assert tmp_W1.shape == ((tmp_N,tmp_V))
+assert tmp_W2.shape == ((tmp_V,tmp_N))
+print(f"tmp_W1.shape: {tmp_W1.shape}")
+print(f"tmp_W2.shape: {tmp_W2.shape}")
+print(f"tmp_b1.shape: {tmp_b1.shape}")
+print(f"tmp_b2.shape: {tmp_b2.shape}")
+def softmax(z):
+    e_z = np.exp(z - np.max(z, axis=0))
+    sum_e_z = np.sum(e_z, axis=0, keepdims=True)
+    yhat = e_z / sum_e_z
+    return yhat
+tmp = np.array([[1,2,3],
+                [1,1,1]
+               ])
+tmp_sm = softmax(tmp)
+display(tmp_sm)
+def forward_prop(x, W1, W2, b1, b2):
+    h = np.dot(W1, x) + b1
+    h = np.maximum(0, h)
+    z = np.dot(W2, h) + b2
+    return z, h
+tmp_N = 2
+tmp_V = 3
+tmp_x = np.array([[0,1,0]]).T
+tmp_W1, tmp_W2, tmp_b1, tmp_b2 = initialize_model(N=tmp_N,V=tmp_V, random_seed=1)
 
-stop_words = open('stopwords','r')
-x = stop_words.readlines()
-_ = 0
-list_of_stop_words = []
-while _ < len(x):
-    if _ < len(x)-1:
-        z=x[_].split('\n')
-        z.pop(1)
-        list_of_stop_words.append(z)
-        _ += 1
-    else:
-        list_of_stop_words.append(x[_])
-        break
+print(f"x has shape {tmp_x.shape}")
+print(f"N is {tmp_N} and vocabulary size V is {tmp_V}")
+tmp_z, tmp_h = forward_prop(tmp_x, tmp_W1, tmp_W2, tmp_b1, tmp_b2)
+print("call forward_prop")
+print()
+print(f"z has shape {tmp_z.shape}")
+print("z has values:")
+print(tmp_z)
+print()
+print(f"h has shape {tmp_h.shape}")
+print("h has values:")
+print(tmp_h)
+def compute_cost(y, yhat, batch_size):
+    logprobs = np.multiply(np.log(yhat),y) + np.multiply(np.log(1 - yhat), 1 - y)
+    cost = - 1/batch_size * np.sum(logprobs)
+    cost = np.squeeze(cost)
+    return cost
+tmp_C = 2
+tmp_N = 50
+tmp_batch_size = 4
+tmp_word2Ind, tmp_Ind2word = get_dict(data)
+tmp_V = len(word2Ind)
 
-# zimportowanie ksiazek w formacie json
-ksiazka_aniol = requests.get('https://wolnelektury.pl/media/book/txt/aniol.txt')
-ksiazka_poetyka = requests.get('https://wolnelektury.pl/media/book/txt/arystoteles-poetyka.txt')
-ksiazka_pies_barkeselvilow = requests.get('https://wolnelektury.pl/media/book/txt/pies-baskervilleow.txt')
-ksiazka_grozny_cien = requests.get('https://wolnelektury.pl/media/book/txt/grozny-cien.txt')
-ksiazka_dolina_trwogi = requests.get('https://wolnelektury.pl/media/book/txt/doyle-dolina-trwogi.txt')
-# utworzenie pliku bag-of-chars
-#bag_of_chars = open('bag-of-chars.txt','w')
-# zmienna place holder ustawiona na wartosc ASCII odpowiadajaca A
-_ = 65
-# zliczenie wystepowania wielkich liter wystepujacych w tekstach i napisanie litery - ilosci
-# while _ <= 90:
-#     bag_of_chars.write(chr(_))
-#     bag_of_chars.write(' - ')
-#     bag_of_chars.write(str(ksiazka_aniol.text.count(chr(_))+
-#         ksiazka_pies_barkeselvilow.text.count(chr(_))+
-#         ksiazka_grozny_cien.text.count(chr(_))+
-#         ksiazka_poetyka.text.count(chr(_))+
-#         ksiazka_dolina_trwogi.text.count(chr(_))))
-#     bag_of_chars.writelines('\n')
-#     _+=1
-# _ = 97
-# # zliczenie wystepowania malych liter wystepujacych w tekstach i napisanie litery - ilosci
-# while _ <= 122:
-#     bag_of_chars.write(chr(_))
-#     bag_of_chars.write(' - ')
-#     bag_of_chars.write(str(ksiazka_aniol.text.count(chr(_))+
-#         ksiazka_pies_barkeselvilow.text.count(chr(_))+
-#         ksiazka_grozny_cien.text.count(chr(_))+
-#         ksiazka_poetyka.text.count(chr(_))+
-#         ksiazka_dolina_trwogi.text.count(chr(_))))
-#     bag_of_chars.writelines('\n')
-#     _+=1
+tmp_x, tmp_y = next(get_batches(data, tmp_word2Ind, tmp_V,tmp_C, tmp_batch_size))
 
-#bag_of_words = open('bag-of-words.txt','w')
+print(f"tmp_x.shape {tmp_x.shape}")
+print(f"tmp_y.shape {tmp_y.shape}")
 
-# podzial ksiazek na slowa ( rozdzielenie przez spacje formanty tekstu oraz utworzenie listy slow )
-slowa_aniol = re.split(r'[\r\n\s\.\,\;\:]',ksiazka_aniol.text)
-slowa_poetyka = re.split(r'[\r\n\s\.\,\;\:]',ksiazka_poetyka.text)
-slowa_pies_bareselvilow = re.split(r'[\r\n\s\.\,\;\:]',ksiazka_pies_barkeselvilow.text)
-slowa_dolina_trwogi = re.split(r'[\r\n\s\.\,\;\:]',ksiazka_dolina_trwogi.text)
-slowa_grozny_cien = re.split(r'[\r\n\s\.\,\;\:]',ksiazka_grozny_cien.text)
-# utworzenie slownikow z czestoscia wystepowania
-slownik_aniol = {}
-slownik_poetyka = {}
-slownik_pies_bareselvilow = {}
-slownik_dolina_trwogi = {}
-slownik_grozny_cien = {}
-for slowo in slowa_aniol:
-    slownik_aniol[slowo] = 0
-for slowo in slowa_poetyka:
-    slownik_poetyka[slowo] = 0
-for slowo in slowa_pies_bareselvilow:
-    slownik_pies_bareselvilow[slowo] = 0
-for slowo in slowa_dolina_trwogi:
-    slownik_dolina_trwogi[slowo] = 0
-for slowo in slowa_grozny_cien:
-    slownik_grozny_cien[slowo] = 0
+tmp_W1, tmp_W2, tmp_b1, tmp_b2 = initialize_model(tmp_N,tmp_V)
 
+print(f"tmp_W1.shape {tmp_W1.shape}")
+print(f"tmp_W2.shape {tmp_W2.shape}")
+print(f"tmp_b1.shape {tmp_b1.shape}")
+print(f"tmp_b2.shape {tmp_b2.shape}")
 
-for slowo in slowa_aniol:
-    if slowo in list_of_stop_words: continue
-    slownik_aniol[slowo] = slowa_aniol.count(slowo)
-for slowo in slowa_poetyka:
-    if slowo in list_of_stop_words: continue
-    slownik_poetyka[slowo] = slowa_poetyka.count(slowo)
-for slowo in slowa_pies_bareselvilow:
-    if slowo in list_of_stop_words: continue
-    slownik_pies_bareselvilow[slowo] = slowa_pies_bareselvilow.count(slowo)
-for slowo in slowa_dolina_trwogi:
-    if slowo in list_of_stop_words: continue
-    slownik_dolina_trwogi[slowo] = slowa_dolina_trwogi.count(slowo)
-for slowo in slowa_grozny_cien:
-    if slowo in list_of_stop_words: continue
-    slownik_grozny_cien[slowo] = slowa_grozny_cien.count(slowo)
-del slownik_aniol['']
-del slownik_poetyka['']
-del slownik_pies_bareselvilow['']
-del slownik_dolina_trwogi['']
-del slownik_grozny_cien['']
-# all_dicts = [
-#      slownik_aniol,
-#      slownik_poetyka,
-#      slownik_pies_bareselvilow,
-#      slownik_dolina_trwogi,
-#      slownik_grozny_cien
-#  ]
-# counters = [Counter(dictionary) for dictionary in all_dicts]
-# merged_counter = counters[0]
-# for counter in counters[1:]:
-#     merged_counter.update(counter)
-# korpus = dict(merged_counter)
-# korpus = dict(sorted(korpus.items(), key= lambda item: item[1],reverse=True))
-# for key, value in korpus.items():
-#     bag_of_words.write(f'{key}:{value}\n')
+tmp_z, tmp_h = forward_prop(tmp_x, tmp_W1, tmp_W2, tmp_b1, tmp_b2)
+print(f"tmp_z.shape: {tmp_z.shape}")
+print(f"tmp_h.shape: {tmp_h.shape}")
 
-def term_frequency(dictionary):
-    sum_values = 0
-    for key,value in dictionary.items():
-        sum_values += value
-    tf = {}
-    for key,value in dictionary.items():
-        tf[key] = value/sum_values
-    return tf
+tmp_yhat = softmax(tmp_z)
+print(f"tmp_yhat.shape: {tmp_yhat.shape}")
 
-def inverse_document_frequency(dictionary,d2,d3,d4,d5):
-    file = open('bag-of-words.txt','r')
-    idf = {}
-    occurence_in_files = 1
-    for key,value in dictionary.items():
-        if key in d2.keys():occurence_in_files  += 1
-        if key in d3.keys(): occurence_in_files += 1
-        if key in d4.keys(): occurence_in_files += 1
-        if key in d5.keys(): occurence_in_files += 1
-        idf[key] = abs(np.log( 5 / occurence_in_files))
-        occurence_in_files = 1
-    return idf
-def tf_idf(dictionary,tf,idf):
-    tf_wartosci = list(tf.values())
-    idf_wartosci = list(idf.values())
-    ptr = {}
-    _ = 0
-    for key , value in dictionary.items():
-        ptr[key] =tf_wartosci[_] * idf_wartosci[_]
-        _ += 1
-    _ = 0
-    return ptr
+tmp_cost = compute_cost(tmp_y, tmp_yhat, tmp_batch_size)
+print("call compute_cost")
+print(f"tmp_cost {tmp_cost:.4f}")
+def back_prop(x, yhat, y, h, W1, W2, b1, b2, batch_size):
+    l1 = np.dot(W2.T, (yhat - y))
+    l1[h <= 0] = 0
+    grad_W1 = np.dot(l1, x.T) / batch_size
+    grad_W2 = np.dot((yhat - y), h.T) / batch_size
+    grad_b1 = np.sum(l1, axis=1, keepdims=True) / batch_size
+    grad_b2 = np.sum((yhat - y), axis=1, keepdims=True) / batch_size
+    return grad_W1, grad_W2, grad_b1, grad_b2
+tmp_C = 2
+tmp_N = 50
+tmp_batch_size = 4
+tmp_word2Ind, tmp_Ind2word = get_dict(data)
+tmp_V = len(word2Ind)
+tmp_x, tmp_y = next(get_batches(data, tmp_word2Ind, tmp_V,tmp_C, tmp_batch_size))
 
-def draw_zipf_graph(dictionary,nazwa : string):
-    dictionary = dict(sorted(dictionary.items(), key= lambda item:item[1], reverse= True))
-    del dictionary['—']
-    keys = list(dictionary.keys())
-    values = list(dictionary.values())
-    ptr = []
-    ptr1 = []
-    for _ in range(10):
-        ptr.append(values[_]/(_+1))
-        ptr1.append(keys[_])
-    plt.plot(ptr1, ptr)
-    plt.title(f'Zipf Graph')
-    plt.xlabel('Rank')
-    plt.ylabel('Frequency')
-    plt.savefig('wykresy/' + nazwa)
-    plt.close()
+print("get a batch of data")
+print(f"tmp_x.shape {tmp_x.shape}")
+print(f"tmp_y.shape {tmp_y.shape}")
 
-draw_zipf_graph(slownik_aniol,'aniol')
-draw_zipf_graph(slownik_poetyka,'poetyka')
-draw_zipf_graph(slownik_pies_bareselvilow,'pies')
-draw_zipf_graph(slownik_grozny_cien,'grozny')
-draw_zipf_graph(slownik_dolina_trwogi,'dolina')
+print()
+print("Initialize weights and biases")
+tmp_W1, tmp_W2, tmp_b1, tmp_b2 = initialize_model(tmp_N,tmp_V)
 
+print(f"tmp_W1.shape {tmp_W1.shape}")
+print(f"tmp_W2.shape {tmp_W2.shape}")
+print(f"tmp_b1.shape {tmp_b1.shape}")
+print(f"tmp_b2.shape {tmp_b2.shape}")
 
-aniol_tf = term_frequency(slownik_aniol)
-grozny_tf = term_frequency(slownik_grozny_cien)
-pies_tf = term_frequency(slownik_pies_bareselvilow)
-dolina_tf = term_frequency(slownik_pies_bareselvilow)
-poetyka_tf = term_frequency(slownik_poetyka)
+print()
+print("Forwad prop to get z and h")
+tmp_z, tmp_h = forward_prop(tmp_x, tmp_W1, tmp_W2, tmp_b1, tmp_b2)
+print(f"tmp_z.shape: {tmp_z.shape}")
+print(f"tmp_h.shape: {tmp_h.shape}")
 
+print()
+print("Get yhat by calling softmax")
+tmp_yhat = softmax(tmp_z)
+print(f"tmp_yhat.shape: {tmp_yhat.shape}")
 
-aniol_idf = inverse_document_frequency(slownik_aniol,slownik_poetyka,slownik_pies_bareselvilow,slownik_grozny_cien,slownik_dolina_trwogi)
-grozny_idf = inverse_document_frequency(slownik_grozny_cien,slownik_poetyka,slownik_pies_bareselvilow,slownik_aniol,slownik_dolina_trwogi)
-pies_idf = inverse_document_frequency(slownik_pies_bareselvilow,slownik_poetyka,slownik_aniol,slownik_grozny_cien,slownik_dolina_trwogi)
-dolina_idf = inverse_document_frequency(slownik_dolina_trwogi,slownik_poetyka,slownik_pies_bareselvilow,slownik_grozny_cien,slownik_aniol)
-poetyka_idf = inverse_document_frequency(slownik_poetyka,slownik_aniol,slownik_pies_bareselvilow,slownik_grozny_cien,slownik_dolina_trwogi)
+tmp_m = (2*tmp_C)
+tmp_grad_W1, tmp_grad_W2, tmp_grad_b1, tmp_grad_b2 = back_prop(tmp_x, tmp_yhat, tmp_y, tmp_h, tmp_W1, tmp_W2, tmp_b1, tmp_b2, tmp_batch_size)
 
-# MACIERZE TF_IDF
-aniol_matrix = tf_idf(slownik_aniol,aniol_tf,aniol_idf)
-grozny_matrix = tf_idf(slownik_grozny_cien,grozny_tf,grozny_idf)
-pies_matrix = tf_idf(slownik_pies_bareselvilow,pies_tf,pies_idf)
-#dolina_matrix = tf_idf(slownik_dolina_trwogi,dolina_tf,dolina_idf)
-poetyka_matrix = tf_idf(slownik_poetyka,poetyka_tf,poetyka_idf)
+print()
+print("call back_prop")
+print(f"tmp_grad_W1.shape {tmp_grad_W1.shape}")
+print(f"tmp_grad_W2.shape {tmp_grad_W2.shape}")
+print(f"tmp_grad_b1.shape {tmp_grad_b1.shape}")
+print(f"tmp_grad_b2.shape {tmp_grad_b2.shape}")
+def gradient_descent(data, word2Ind, N, V, num_iters, alpha=0.03):
+    W1, W2, b1, b2 = initialize_model(N,V, random_seed=282)
+    batch_size = 128
+    iters = 0
+    C = 2
+    for x, y in get_batches(data, word2Ind, V, C, batch_size):
+        z, h = forward_prop(x, W1, W2, b1, b2)
+        yhat = softmax(z)
+        cost = -np.sum(y * np.log(yhat)) / batch_size
+        if ( (iters+1) % 10 == 0):
+            print(f"iters: {iters + 1} cost: {cost:.6f}")
+        grad_W1, grad_W2, grad_b1, grad_b2 = back_prop(x, yhat, y, h, W1, W2, b1, b2, batch_size)
+        W1 = alpha * grad_W1 - W1
+        W2 = alpha * grad_W2 - W2
+        b1 = alpha * grad_b1 - b1
+        b2 = alpha * grad_b2 - b2
+         iters += 1
+        if iters == num_iters:
+            break
+        if iters % 100 == 0:
+            alpha *= 0.66
 
-def odleglosc_cos(matrix1,matrix2):
-    distance = 0
-    sum = 0
-    sumx_squere = 0
-    sumy_squere = 0
-    matrix1_values = list(matrix1.values())
-    matrix2_values = list(matrix2.values())
-    if len(matrix1_values) > len(matrix2_values):
-        for i in range(len(matrix2_values)):
-            sum += matrix1_values[i]*matrix2_values[i]
-            sumx_squere += pow(matrix1_values[i],2)
-            sumy_squere += pow(matrix2_values[i],2)
-    else:
-        for i in range(len(matrix1_values)):
-            sum += matrix1_values[i] * matrix2_values[i]
-            sumx_squere += pow(matrix1_values[i], 2)
-            sumy_squere += pow(matrix2_values[i], 2)
-    mianownik = (math.sqrt(sumx_squere)*math.sqrt(sumy_squere))
-    if mianownik != 0:
-        distance = sum/mianownik
-    else:
-        distance = sum/1
-    return distance
+    return W1, W2, b1, b2
+C = 2
+N = 50
+word2Ind, Ind2word = get_dict(data)
+V = len(word2Ind)
+num_iters = 150
+print("Call gradient_descent")
+W1, W2, b1, b2 = gradient_descent(data, word2Ind, N, V, num_iters)
+from matplotlib import pyplot
+%config InlineBackend.figure_format = 'svg'
+words = ['king', 'queen','lord','man', 'woman','dog','wolf',
+         'rich','happy','sad']
 
-aniol_grozny_od = odleglosc_cos(aniol_matrix,grozny_matrix)
-aniol_pies_od = odleglosc_cos(aniol_matrix,pies_matrix)
-aniol_poetyka_od = odleglosc_cos(aniol_matrix,poetyka_matrix)
-
-grozny_pies_od = odleglosc_cos(grozny_matrix, pies_matrix)
-grozny_poetyka_od = odleglosc_cos(grozny_matrix, poetyka_matrix)
-
-pies_poetyka_od = odleglosc_cos(pies_matrix,poetyka_matrix)
-
-odleglosci = [[None,'aniol','grozny','pies','poetyka'],
-              ['aniol',None,aniol_grozny_od.__round__(2),aniol_pies_od.__round__(2),aniol_poetyka_od.__round__(2)],
-              ['grozny',aniol_grozny_od.__round__(2),None,grozny_pies_od.__round__(2),grozny_poetyka_od.__round__(2)],
-              ['pies',aniol_pies_od.__round__(2),grozny_pies_od.__round__(2),None,pies_poetyka_od.__round__(2)],
-              ['poetyka',aniol_poetyka_od.__round__(2),grozny_poetyka_od.__round__(2),pies_poetyka_od.__round__(2),None]]
-
-for x in odleglosci:
-    print(x,'\n')
-
+embs = (W1.T + W2)/2.0
+idx = [word2Ind[word] for word in words]
+X = embs[idx, :]
+print(X.shape, idx)
+result= compute_pca(X, 2)
+pyplot.scatter(result[:, 0], result[:, 1])
+for i, word in enumerate(words):
+    pyplot.annotate(word, xy=(result[i, 0], result[i, 1]))
+pyplot.show()
+result= compute_pca(X, 4)
+pyplot.scatter(result[:, 3], result[:, 1])
+for i, word in enumerate(words):
+    pyplot.annotate(word, xy=(result[i, 3], result[i, 1]))
+pyplot.show()
